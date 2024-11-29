fb9c1d5de06b3f52dc08bfbff55d259173d81b2b
vishalprajapat <113760119+VISHALKMP@users.noreply.github.com>
2024-11-29 05:42:15 +0000

    learned new more topics

---
+++ 01_Basics/map1.js
@@ -0,0 +1,7 @@
+let map = new Map();
+map.set('IN',"sher")
+map.set('GE',"Batman")
+map.set('Fr',"Davinchi")
+
+
+console.log(map);
---
+++ 01_Basics/while1.js
@@ -0,0 +1,12 @@
+var index = 0;
+while (index <=20) {
+    // console.log(`value of index is ${index}`);
+   index = index+2;
+}
+
+let Arrays = ['hero','sherr','batman','pokemon']
+let arr = 0;
+while( arr <Arrays.length){
+      console.log(`value is ${Arrays[arr]}`)
+      arr = arr+1
+}
