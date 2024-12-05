e8288b9edcd68637f4381841152ff67c60a5ba11
vishalprajapat <113760119+VISHALKMP@users.noreply.github.com>
2024-12-05 06:10:51 +0000

    made todo taked help from apna collage

---
+++ 01_Basics/tododom.js
@@ -0,0 +1,29 @@
+let btn = document.querySelector('button');
+let inp = document.querySelector("input")
+let ul = document.querySelector("ul");
+
+
+btn.addEventListener("click" , function (){
+
+    let item = document.createElement('li')
+    item.innerText = inp.value;
+    item.setAttribute("style","color:black; font-size:36px;")
+let delBtn = document.createElement('button');
+delBtn.innerText = "delete";
+delBtn.classList.add("delete");
+
+item.appendChild(delBtn);
+    ul.appendChild(item);
+    inp.value = "";   
+})
+
+let btndel = document.querySelectorAll('.delete');
+for(delBtn of btndel){
+    delBtn.addEventListener("click", function (){
+     let par = this.parentElement;
+     console.log(par);
+     par.remove();
+     
+    })
+}
+ 
